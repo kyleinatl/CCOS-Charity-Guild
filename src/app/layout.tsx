f299efa8f2f08b0d96832f1c7f1008a1b3ac06e2
kyleinatl <concordefire20@gmail.com>
2026-01-06 21:04:56 +0000

    Add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, Montserrat } from "next/font/google";
 import { AuthProvider } from "@/lib/auth/auth-context";
+import Script from "next/script";
 import "./globals.css";
 
 const bebasNeue = Bebas_Neue({
@@ -48,6 +49,25 @@ export default function RootLayout({
         className={`${bebasNeue.variable} ${montserrat.variable} antialiased`}
       >
         {children}
+        
+        {/* Tawk.to Live Chat */}
+        <Script
+          id="tawk-to-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/695d78b01b9b1e197b6103a6/1jeahusip';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
